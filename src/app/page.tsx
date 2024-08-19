6b54206146c00303f5b519f68a99802e4c4d40c2
Joshua Cano <joshuacano@Joshuas-MBP.attlocal.net>
2024-08-19 00:38:51 -0500

    Initial convex overview

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,33 @@
 "use client";
 
 import { SignInButton, UserButton } from "@clerk/nextjs";
-import { Authenticated, Unauthenticated } from "convex/react";
+import {
+  Authenticated,
+  Unauthenticated,
+  useMutation,
+  useQuery,
+} from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+  const documents = useQuery(api.documents.getDocuments);
+  const createDocument = useMutation(api.documents.createDocument);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {" "}
       <Unauthenticated>
         <SignInButton />
       </Unauthenticated>
       <Authenticated>
         <UserButton />
+
+        <button
+          onClick={() => {
+            createDocument({ title: "hello world" });
+          }}
+        >
+          Click Me
+        </button>
+        {documents?.map((doc) => <div key={doc._id}>{doc.title}</div>)}
       </Authenticated>
     </main>
   );
